78020e8133875085bef2994cac32acd80c694da7
MercyMW <elizabethwangui895@gmail.com>
2022-05-18 11:13:53 +0300

    MY README FILE

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# Inspire-In-STEM
-# Inspire-In-STEM
-# Inspire-In-STEM
-# Inspire-In-STEM
-# inspire_in_stem2
+### inspire youth in stem
+
+## Week 1 : INTRODUCTION TO PYTHON
+Day 1 : installing python 
+Add a link to downloading python
+
+Add a link to downloading VS Code
+
+Add a link to downloading GIT Windows
+
+./
+
+## Week 2 Electronics for innovators
+
+
+## Week 3 : Introduction to Robotics
+
+
+## Week 4 : JavaScript, CSS, HTML
+
+
+## Week 5 : Design Thinking, UX, UI
+
+
+## Week 6 : Critical Division, Campus Tour
--- lesson5.py
+++ lesson5.py
@@ -1,31 +1,76 @@
+# methods
+name = "Muiruri Mercy"
+age = 17
+person = "I am " + str(name) + " and my age is " + str(age)
+print(person)
+
+print("i am " + name)
+
+# format method
+print( " My name is {} and i am {} years old ".format( name,age ))
+print(f"My name is {name} and i am {age} years old") 
+print(f" My name is \t {name} \n and i am {age}")
+
+user_name = " remove all spaces "
+print(user_name)
+print(user_name.strip())
+user = " the space on the right will be removed "
+print(user.rstrip())
+usert= " the space on the left will be removed "
+print(usert.lstrip())
+
+msg = ''' QRSTLAK12 Mpesa confirmed 
+          you have received 100000 from ANGELA MURIUKI
+          Safaricom simple,transparent for you'''
+print(msg)
+
+multipleLineString = """ You can also print a string
+                         multiple line string like this using
+                        three single qoutation marks or
+                        three double qoutation marks """
+print(multipleLineString)
+
+city = "Nairobi" 
+print(city[:5])
+print(city[2:])
+print(city[-1])
+
+# converting to uppercase or lowercase
+f_name = "mercy muiruri"
+print(f_name.upper())
+s_name = "MERCY MUIRURI"
+print(s_name.lower())
+m_name = "Mercy Muiruri"
+print(m_name.upper())
+print(m_name.lower())
+
+# concatenation converting one data to another
+# integer to float
+number = 7
+print(float(number))
+# floats are numbers with decimals
+
+# integer to string
+number = 14
+print(str(number))
+# here the number is printed as a string
+
+#float to integer
+number = 12.34
+print(int(number))
+# here it prints out the whole number or integer only
+
+f_name = "Metro"
+s_name = "Sacco"
+full_name = f_name + s_name
+print(full_name)
+
+# The .replace method replaces a specific character with another
+name = "Brett Cooper"
+print(name.replace('t','g'))
 
 
-# volume of a cube
+msg = "Hello from Mercy Muiruri welcome"
+print(msg.split())
 
-length = input(" Enter the length ")
-volumeOfACube = int(length) * int(length) * int(length)
-
-print(" The volume of a cube is" + str(volumeOfACube))
-
-# surface area of a cylinder
-radius = input( enter the radius of the circle)
-diameter = int(radius) * 2
-height = input( enter the height of the circle)
-PI = 3.142
-surfaceAreaOfACylinder = 2 * PI * int(radius) * int(radius) + PI * diameter * int(height)
-print("The area of a circle is " + str(surfaceAreaOfACylinder))
-
-# volume of a cube
-
-# area of a circle
-
-radius = input(" Enter the radius of the cirlce")
-PI = 3.142
-areaOfACircle = PI * int(radius) * int(radius)
-
-print(" Area of a circle is" + str(areaOfACircle))
-
-
-
-
-
+print(len(msg))
