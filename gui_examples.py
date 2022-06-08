75c41ed5a98f9f32d4cb89eb1ad8dc68c72b2c5a
MercyMW <elizabethwangui895@gmail.com>
2022-06-08 20:54:00 +0300

    user_interfaces

--- gui_examples.py
+++ gui_examples.py
@@ -23,20 +23,28 @@ s_name = Label(window, text="SECOND NAME",font=("Calibri Bold",20))
 f_name.grid(column = 60,row = 100)    # this specifys where exactly on the window it will print out
 s_name.grid(column = 60,row = 120)
 
+
+
 def open_poppup():
     top = Toplevel(window)
     top.geometry("300x300")
     top.title(" POPPUP WINDOW")
-    top.configure(bg="purple")
-    msg = Label(top,text ="Welcome user!",font = ("Arial",18))
+    top.configure(bg="white")
+    Label(top,text ="Welcome user!\n Have a great Day",font = ("Arial",20)).place(x= 75,y=100) # when you want to show the place on the grid, 
+#if you want to use the words column anmd row,use grid, if not, just say x(column) and y(grid)
+    Button(top,text = "Inspirational",bg = "red").place(x=100,y=200)
+    Button(top,text = "Comical",bg= "green").place(x=250,y=200)
 
-btn = Button(window,text = "Click here to start",bg="red",fg="green",command = open_poppup()) # this creates a button to click and specifys the background colour and foreground colour
-btn.grid(column = 120,row = 150)    #this just specifys on the grid where the button is to be
 
 f_name_box = Entry(window,width=20,bg="green")
 f_name_box.grid(column = 100,row = 100)
 s_name_box = Entry(window,width=20,bg="green")
 s_name_box.grid(column = 100,row = 120)
+
+btn = Button(window,text = "Click here to start",bg="red",fg="green",command = open_poppup)# this creates a button to click and specifys the background colour and foreground colour
+btn.grid(column = 120,row = 150)   #this just specifys on the grid where the button is to be
+
+
 
 
 
